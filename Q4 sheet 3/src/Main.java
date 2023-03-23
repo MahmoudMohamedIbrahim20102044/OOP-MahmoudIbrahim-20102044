0344e3d0833e9f08cafa2ed504fc6324cb52be7a
MahmoudMohamedIbrahim20102044 <128157273+MahmoudMohamedIbrahim20102044@users.noreply.github.com>
2023-03-23 23:16:14 +0200

    Add files via upload

---
+++ Q4 sheet 3/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Triangle t=new Triangle(true,"Blue",5,3,4);
+        System.out.println(t.getArea()+"  "+t.getPerimeter()+"  "+t.color+"  "+t.isFilled);
+    }
+
+}
